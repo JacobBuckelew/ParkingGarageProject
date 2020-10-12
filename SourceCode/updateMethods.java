e6277caf25658859b8c0a2ebd2dbf282fa38cbad
JacobBuckelew <46821835+JacobBuckelew@users.noreply.github.com>
2020-10-12 18:07:10 -0400

    Create updateMethods.java

---
+++ SourceCode/updateMethods.java
@@ -0,0 +1,8 @@
+/*
+ * The updateMethods interface contains a shareable method updateEventStatus to update Vehicles and ParkingSpots.
+ */
+public interface updateMethods {
+	
+	public void updateEventStatus();
+
+}
